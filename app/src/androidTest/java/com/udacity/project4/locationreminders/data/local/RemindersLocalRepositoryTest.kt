7a7b76a7556012327d7401d069d68ceb256e9ad0
Amr Hany <amr.hany1@outlook.com>
2023-03-09 19:42:42 +0200

    Add RemindersLocalRepo tests

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -5,15 +5,18 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsEqual
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -25,6 +28,61 @@ import org.junit.runner.RunWith
 @MediumTest
 class RemindersLocalRepositoryTest {
 
-//    TODO: Add testing implementation to the RemindersLocalRepository.kt
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var localDataSource: ReminderDataSource
+    private lateinit var database: RemindersDatabase
+
+    @Before
+    fun setup() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            RemindersDatabase::class.java
+        )
+            .allowMainThreadQueries()
+            .build()
+
+        localDataSource =
+            RemindersLocalRepository(
+                database.reminderDao(),
+                Dispatchers.Main
+            )
+    }
+
+    @After
+    fun tearDownTests() = database.close()
+
+    @Test
+    fun saveReminder_retrievesReminder() = runBlockingTest {
+        // GIVEN - A new reminder saved in the database.
+        val newReminder = ReminderDTO("TestTitle0", null, "TestLocation1", 3.6, 3.4)
+        localDataSource.saveReminder(newReminder)
+
+        // WHEN  - Reminder retrieved by ID.
+        val result = localDataSource.getReminder(newReminder.id)
+
+        // THEN - Same reminder is returned.
+        result as Result.Success
+        assertThat<ReminderDTO>(result as ReminderDTO, notNullValue())
+        assertThat(result.id, `is`(newReminder.id))
+        assertThat(result.title, `is`(newReminder.title))
+        assertThat(result.description, `is`(newReminder.description))
+    }
+
+    @Test
+    fun deleteReminders_retrievesReminders() = runBlocking {
+        // GIVEN - Clear the DB
+        localDataSource.deleteAllReminders()
+
+        // WHEN  - Retrieve all reminders
+        val result = localDataSource.getReminders()
+
+        // THEN - the list is empty
+        result as Result.Success
+        assertThat(result.data.size, IsEqual(0))
+    }
+
+
 
 }
