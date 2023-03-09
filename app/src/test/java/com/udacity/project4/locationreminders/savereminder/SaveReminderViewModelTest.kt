dbcb23f6f7343102590943d52cccf879f455db83
Amr Hany <amr.hany1@outlook.com>
2023-03-09 19:18:17 +0200

    Add tests for save reminder viewmodel

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,56 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert
+import org.hamcrest.core.IsEqual
+import org.hamcrest.core.IsNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    private lateinit var repo: FakeDataSource
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
 
+    @Before
+    fun setup() {
+        repo = FakeDataSource()
+        saveReminderViewModel =
+            SaveReminderViewModel(ApplicationProvider.getApplicationContext(), repo)
+    }
+
+    @Test
+    fun validateAndSaveReminder_nullDescription_IsNotValid() {
+        //Given a reminder without a description
+        val reminder = ReminderDataItem("TestTitle0", null, "TestLocation1", 3.6, 3.4)
+        val isValid = saveReminderViewModel.validateEnteredData(reminder)
+
+        // the returned value isnt valid
+        MatcherAssert.assertThat(isValid, `is`(true))
+    }
+
+    @Test
+    fun clearSelectedLocation() {
+        //Given that we clear the location
+        saveReminderViewModel.clearSelectedLocation()
+        //The lat and long is null
+        MatcherAssert.assertThat(saveReminderViewModel.longitude.value, IsNull())
+        MatcherAssert.assertThat(saveReminderViewModel.longitude.value, IsNull())
+    }
 
 }
